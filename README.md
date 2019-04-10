1d9dbae6008eb3bab5c9f99194d1eed790a572e0
cguisheng <cguisheng@linewell.com>
2019-04-10 08:09:07 +0800

    序列化 反序列化

--- README.md
+++ README.md
@@ -15,8 +15,7 @@ avro的java实现还依赖了[Jackson](http://wiki.fasterxml.com/JacksonDownload
       <version>1.8.2</version>
     </dependency>
           
-    以下是avro的maven插件 (用于执行代码生成):
-    
+    <!-- 以下是avro的maven插件 (用于执行代码生成): -->
     <plugin>
       <groupId>org.apache.avro</groupId>
       <artifactId>avro-maven-plugin</artifactId>
@@ -69,7 +68,7 @@ avro模式使用json定义，模式由简单类型（null, boolean, int, long, f
 
 #### 代码生成
 ###### 使用代码生成进行序列化或反序列化
-编译schema为java类（代码生成）：
+* 编译schema为java类（代码生成）：
     
      1. 指定目录：
         java -jar C:\Users\cguisheng\Desktop\avro-tools-1.8.2.jar compile schema D:\workspaces\avrotest\avro\user.avsc D:\workspaces\avrotest\src\main\java\       
@@ -84,4 +83,54 @@ avro模式使用json定义，模式由简单类型（null, boolean, int, long, f
     1. 通过构造函数
     2. 通过builder构造器  
     
-    区别：通过构造函数创建对象性能优于builder（builder创建对象实际也是先调用了构造函数创建对象）。通过builder创建对象时，
+    
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+        // 此时 color null
+        
+        // 带参构造
+        User user2 = new User("Ben", 7, "red");
+        
+        // 通过 builder
+        User user3 = User.newBuilder()
+                     .setName("Charlie")
+                     .setFavoriteColor("blue")
+                     .setFavoriteNumber(null)
+                     .build();
+    
+    区别：通过构造函数创建对象性能优于builder（builder创建对象实际也是先调用了构造函数创建对象）。通过builder创建对象时，每个属性都必须显式设置，即使该属性为null。
+    如：user1的favoriteColor并没有被赋值，但是user3的favoriNumber必须被显式地设置为null否则报错。
+
+* 序列化
+
+        // 序列化 user1, user2 和 user3 到磁盘
+        // DatumWriter类：将Java对象转换为内存中的序列化格式
+        // SpecificDatumWriter 类：和生成的类（user）一起使用，并从生成的类中提取schema
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        
+        // DataFileWriter类：将序列化记录以及模式写入dataFileWriter.create(user1.getSchema(), new File("users.avro"))方法调用中指定的文件。
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+        
+        // 通过调用dataFileWriter.append方法将用户写入文件。
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        // 完成写操作时，关闭数据文件。
+        dataFileWriter.close();
+
+
+* 反序列化
+
+        // 从磁盘中反序列化user，与序列化的操作类似
+        // SpecificDatumWriter类：将内存中序列化的项转换成我们生成的类的实例
+        DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+        // 将userDatumReader和之前生成的users.avro文件传递给DataFileReader，读取磁盘中文件数据
+        DataFileReader<User> dataFileReader = new DataFileReader<User>(new File("users.avro"), userDatumReader);
+        User user = null;
+        while (dataFileReader.hasNext()) {
+            // 通过将用户对象传递给next() 来重用它。减少多个项目的文件分配和垃圾回收象。
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
--- src/main/java/com/linewell/test/App.java
+++ src/main/java/com/linewell/test/App.java
@@ -1,6 +1,15 @@
 package com.linewell.test;
 
 import com.linewell.avro.User;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -19,22 +28,45 @@ public class App {
         User user3 = User.newBuilder()
                 .setName("charlie")
                 .setFavoriteColor("blue")
-//                .setFavoriteNumber(null)
+                .setFavoriteNumber(null)
                 .build();
 
+        try {
+            // ========================= 序列化 begin =================================
+            // 序列化 user1, user2 和 user3 到磁盘
+            // DatumWriter类：将Java对象转换为内存中的序列化格式
+            // SpecificDatumWriter 类：和生成的类（user）一起使用，并从生成的类中提取schema
+            DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
 
-        /*here:
-        for (int i=0;i<100;i++) {
-            System.out.println("==== i:"+i);
-            there:
-            for (int j=0;j<100;j++) {
-                System.out.println("j=" + j);
-                if (j == 10) {
-                    break there;
-                }
+            // DataFileWriter类：将序列化记录以及模式写入dataFileWriter.create(user1.getSchema(), new File("users.avro"))方法调用中指定的文件。
+            DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+            dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+
+            // 通过调用dataFileWriter.append方法将用户写入文件。
+            dataFileWriter.append(user1);
+            dataFileWriter.append(user2);
+            dataFileWriter.append(user3);
+            // 完成写操作时，关闭数据文件。
+            dataFileWriter.close();
+            // ========================= 序列化 end =================================
+
+
+            // ========================= 反序列化 begin =================================
+            // 从磁盘中反序列化user，与序列化的操作类似
+            // SpecificDatumWriter类：将内存中序列化的项转换成我们生成的类的实例
+            DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+            // 将userDatumReader和之前生成的users.avro文件传递给DataFileReader，读取磁盘中文件数据
+            DataFileReader<User> dataFileReader = new DataFileReader<User>(new File("users.avro"), userDatumReader);
+            User user = null;
+            while (dataFileReader.hasNext()) {
+                // 通过将用户对象传递给next() 来重用它。减少多个项目的文件分配和垃圾回收象。
+                user = dataFileReader.next(user);
+                System.out.println(user);
             }
-
+            // ========================= 反序列化 end =================================
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("over");*/
+
     }
 }
